9e6a9457fdaf747e7519f51fdd1feb23eee298fb
butthx <68604099+butthx@users.noreply.github.com>
2022-02-17 19:11:56 +0700

    add star button

--- src/modules/start.ts
+++ src/modules/start.ts
@@ -3,39 +3,44 @@ import GModel from '../schema/database';
 import { Composer, Context } from 'grammy';
 import isAdmin from '../utils/isAdmin';
 export const bot = new Composer();
+const start_keyboard: [
+  [
+    {
+      text: '❓ Help';
+      callback_data: 'help';
+    },
+    {
+      text: '🔒 Privacy Policy';
+      callback_data: 'privacy';
+    }
+  ],
+  [
+    {
+      text: '📦 Source';
+      url: 'https://github.com/butthx/nochnbot';
+    },
+    {
+      text: '🧚🏻‍♂️ Support';
+      url: 'https://t.me/butthxdiscuss';
+    }
+  ],
+  [
+    {
+      text: '🗞️ Channel';
+      url: 'https://t.me/butthxforward';
+    },
+    {
+      text: '⭐ Give Star';
+      callback_data: 'star';
+    }
+  ]
+];
 const start_message = `Hi, i can delete message from user which using channel to sending message. also this user banned that channel from your group, so the owner can't use it again for sending message in your group.`;
 bot.callbackQuery('start', (ctx) => {
   return ctx.editMessageText(start_message, {
     parse_mode: 'HTML',
     reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: '❓ Help',
-            callback_data: 'help',
-          },
-          {
-            text: '🔒 Privacy Policy',
-            callback_data: 'privacy',
-          },
-        ],
-        [
-          {
-            text: '📦 Source',
-            url: 'https://github.com/butthx/nochnbot',
-          },
-          {
-            text: '🧚🏻‍♂️ Support',
-            url: 'https://t.me/butthxdiscuss',
-          },
-        ],
-        [
-          {
-            text: '🗞️ Channel',
-            url: 'https://t.me/butthxforward',
-          },
-        ],
-      ],
+      inline_keyboard: start_keyboard,
     },
   });
 });
@@ -45,34 +50,34 @@ bot.command('start', (ctx) => {
     reply_to_message_id: ctx.message?.message_id,
     allow_sending_without_reply: true,
     reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: '❓ Help',
-            callback_data: 'help',
-          },
-          {
-            text: '🔒 Privacy Policy',
-            callback_data: 'privacy',
-          },
-        ],
-        [
-          {
-            text: '📦 Source',
-            url: 'https://github.com/butthx/nochnbot',
-          },
-          {
-            text: '🧚🏻‍♂️ Support',
-            url: 'https://t.me/butthxdiscuss',
-          },
-        ],
-        [
-          {
-            text: '🗞️ Channel',
-            url: 'https://t.me/butthxforward',
-          },
-        ],
-      ],
+      inline_keyboard: start_keyboard,
     },
   });
+});
+bot.callbackQuery('star', async (ctx) => {
+  return ctx.editMessageText(
+    `If you like this bot, please give five star.\nThanks you for using this bot.`,
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: 'Bots Archive',
+              url: 'https://t.me/BotsArchive/2350',
+            },
+            {
+              text: 'Telegramic',
+              url: 'https://t.me/tlgrmcbot?start=nochannel_robot-review',
+            },
+          ],
+          [
+            {
+              text: 'Back',
+              callback_data: 'start',
+            },
+          ],
+        ],
+      },
+    }
+  );
 });
